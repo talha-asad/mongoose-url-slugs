5d4ba33cae5dc18acdb78e4caa5d23639d9417f3
Talha Asad <talha.asad@agiledevs.com>
2014-06-09 16:23:31 +0500

    initial release

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var extend = require('extend'),
-    mongoose = require('mongoose'),
     inspect = require('util').inspect;
 
 function defaultURLSlugGeneration(text) {
@@ -30,11 +29,9 @@ module.exports = function(slugProperty, options) {
     schemaField[options.key] = {type: options.index.type, trim: options.index.trim, index: options.index.index, unique: options.index.unique, required: options.index.required};
     schema.add(schemaField);
 
-    schema.methods.ensureUniqueSlug = function (slug, cb) {
+    schema.methods.ensureUniqueSlug = function (doc, slug, cb) {
       if (!options.index.unique) return cb(null, true);
-      console.log('This: ', inspect(schema, {deep: null}));
-      console.log('Constructor: ', inspect(schema.constructor, {deep: null}));
-      var model = schema.model(schema.constructor.modelName);
+      var model = doc.constructor;
       var q = {};
       q[options.key] = slug;
       model.findOne(q, {_id: 1}).exec(function (e, doc) {
@@ -45,17 +42,17 @@ module.exports = function(slugProperty, options) {
     }
 
     schema.pre('save', function (next) {
-      if (this.get(options.key)) return next();
-      var self = this;
+      var doc = this;
+      if (doc.get(options.key)) return next();
 
       var toSlugify = '';
       if (slugProperty instanceof Array) {
         for (var i = 0; i < slugProperty.length; i++) {
-          toSlugify += this.get(slugProperty[i]) + ' ';
+          toSlugify += doc.get(slugProperty[i]) + ' ';
         }
         toSlugify = toSlugify.substr(0, toSlugify.length-1);
       } else {
-        toSlugify = this.get(slugProperty);
+        toSlugify = doc.get(slugProperty);
       }
 
       function uniqueSlugGeneration(slugCount, cb) {
@@ -65,18 +62,16 @@ module.exports = function(slugProperty, options) {
         }
         slugCount = slugCount || 1;
         var tmpSlug = options.generator(toSlugify + ((slugCount > 1)? ' ' + slugCount : ''));
-        console.log('currentSlug: ', tmpSlug);
-        schema.methods.ensureUniqueSlug(tmpSlug, function (e, unique) {
+        schema.methods.ensureUniqueSlug(doc, tmpSlug, function (e, unique) {
           if (e) cb(e);
           if (!unique) return uniqueSlugGeneration(++slugCount, cb);
           else cb(null, tmpSlug);
         });
       }
       
-      console.log('toSlugify: ', toSlugify);
       uniqueSlugGeneration(function (e, finalSlug) {
         if (e) return next(e);
-        self.set(options.key, finalSlug);
+        doc.set(options.key, finalSlug);
         next();
       });
       
